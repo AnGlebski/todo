d574323b0852844976ff935bdd9169a3dbe9574f
An Glebski <gluhova19962010@mail.ru>
2021-05-20 00:00:53 +0300

    add todosData

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import TodoItem from './components/TodoItem';
+import todosData from './todosData';
 
 
 function App() {
+
+  const todoItems = todosData.map(item => <TodoItem key={item.id}
+    item={item} />)
+
   return(
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todoItems}
     </div>
   )
 }
